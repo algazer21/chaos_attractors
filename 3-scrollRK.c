87825e3c7e85c52aaaa1509d8660efccf19535c4
algazer21 <118944151+algazer21@users.noreply.github.com>
2023-01-17 23:10:16 -0600

    Update 3-scrollRK.c

--- 3-scrollRK.c
+++ 3-scrollRK.c
@@ -1,7 +1,6 @@
 /**********************************************************
-						Alan García Zermeño
-			 Para el curso de Métodos numéricos
-						  CIMAT 5/12/22
+Alan García Zermeño
+5/12/22
 Resuelve el sistema dinámico de Three-Scroll Unified CS
 ***********************************************************/
 #include <stdio.h>
